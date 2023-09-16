fc7b9f25fd620aa326f1482049aa607a3337e142
Olian04 <rift95@live.se>
2023-09-16 21:02:50 +0200

    chore: Remove unused import

--- src/Get.ts
+++ src/Get.ts
@@ -1,5 +1,4 @@
 import { Option, Some, None } from "./Option";
-import { match } from "./match";
 
 export const Get = <V>(object: Partial<Record<string | number | symbol, V>>, key: keyof typeof object): Option<V> => {
     if (key in object) {
