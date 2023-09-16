afb2ef7f0dd1caaca58e0b354f5150746d8996bf
Olian04 <rift95@live.se>
2023-09-16 21:35:23 +0200

    feat: Remove branching in flatten and map

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,18 +1,14 @@
 import { Option, None } from "./Option";
 import { Result, Err } from "./Result";
-import { match } from "./match";
+import { chainFn, chainKey } from "./private";
 
 export function flatten<V>(outerOption: Option<Option<V>>): Option<V>;
 export function flatten<V, E>(outerResult: Result<Result<V, E>, E>): Result<V, E>;
 export function flatten<V, E>(target: Option<Option<V>> | Result<Result<V, E>, E>) {
-    if ('isSome' in target) {
-        return match(target, {
-            Some: innerOption => innerOption,
-            None: () => None,
-        });
-    }
-    return match(target, {
-        Ok: innerResult => innerResult,
-        Err: err => Err(err),
-    });
+    return target[chainFn]({
+        Some: v => v,
+        None: () => None,
+        Ok: v => v,
+        Err: e => Err(e),
+    }[target[chainKey]]);
 }
--- src/map.ts
+++ src/map.ts
@@ -1,25 +1,21 @@
 import { None, Option, Some } from './Option';
 import { Err, Ok, Result } from './Result';
-import { match } from './match';
+import { chainFn, chainKey } from './private';
 
 export function map<V, T>(option: Option<V>, fn: (value: V) => T): Option<T>;
 export function map<V, E, T>(result: Result<V, E>, fn: (value: V) => T): Result<T, E>;
 export function map(target: Option<unknown> | Result<unknown, unknown>, fn: (val: unknown) => unknown) {
-    if ('isSome' in target) {
-        return match(target, {
-            Some: v => Some(fn(v)),
-            None: () => None,
-        });
-    }
-    return match(target, {
+    return target[chainFn]({
+        Some: v => Some(fn(v)),
+        None: () => None,
         Ok: v => Ok(fn(v)),
         Err: e => Err(e),
-    });
+    }[target[chainKey]]);
 }
 
 export function mapErr<V, E, F>(result: Result<V, E>, fn: (error: E) => F): Result<V, F> {
-    return match(result, {
+    return result[chainFn]({
         Ok: v => Ok(v),
         Err: e => Err(fn(e)),
-    });
+    }[result[chainKey]]);
 }
